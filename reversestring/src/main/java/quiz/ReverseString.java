df0b6375b797a2fce44a38d859378ec1704ec5fc
Matthew McCormick <matthew.o.mccormick@gmail.com>
2017-12-20 10:31:16 -0500

    Added another reverse method using substring.  It works, but staying with StringBuffer.append()

--- reversestring/src/main/java/quiz/ReverseString.java
+++ reversestring/src/main/java/quiz/ReverseString.java
@@ -4,6 +4,7 @@ public class ReverseString {
     public String reverse(String input) {
         //return new StringBuffer(input).reverse().toString();
     	return reverseViaForLoop(input);
+    	//return reverseViaSubstringLoop(input);
     }
     
     private String reverseViaForLoop(String input) {
@@ -14,5 +15,15 @@ public class ReverseString {
     	}
     	
     	return reverseBuffer.toString();
+    }
+    
+    private String reverseViaSubstringLoop(String input) {
+    	String reverseString = new String();
+    	
+    	for(int i = 0; i < input.length(); i++) {
+    		reverseString = input.substring(i,i+1) + reverseString;
+    	}
+    	
+    	return reverseString;
     }
 }
